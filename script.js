2a167574a209123f8dee83322438442148196596
LA0105 <lakshmi1.ntrj@gmail.com>
2026-02-14 17:12:03 +0530

    script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function fillLoveMeter() {
   document.getElementById('loveFill').style.width = '100%';
 }
 
-const countdownDate = new Date("Mar 19, 2026 00:00:00").getTime();
+const specialDate = new Date("Mar 19, 2026 17:00:00").getTime();
 function updateCountdown() {
   const now = new Date().getTime();
   const dist = countdownDate - now;
